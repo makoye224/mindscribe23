d39419870844d7848fd6dce369199198519ccc65
Emmanuel Makoye <makoye224@gmail.com>
2023-10-20 20:37:00 -0400

    static files collected

--- storefront/urls.py
+++ storefront/urls.py
@@ -19,16 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
 
-admin.site.site_header = "Sokoni Administration"
+admin.site.site_header = "MindScribe Administration"
 admin.site.index_title = "Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("playground/", include("playground.urls")),
-    path("store/", include("store.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
-    path("__debug__/", include(debug_toolbar.urls)),
     path("", include("core.urls")),
 ]
 
