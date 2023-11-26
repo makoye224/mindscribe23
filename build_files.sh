76feaa84b28705069c3ffccfa48999d8c0bf6ce7
Emmanuel Makoye <makoye224@gmail.com>
2023-11-25 22:41:22 -0500

    kanye

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Run Django migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Optionally, you may run other commands, such as compressing assets or creating a production-ready build.
+
+# Move the collected static files to the desired directory
+mv static staticfiles_build
