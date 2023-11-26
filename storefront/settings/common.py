1450a4e41d8e9f297476d481e4f1d23c41c178ca
Emmanuel Makoye <makoye224@gmail.com>
2023-11-25 23:19:38 -0500

    bb

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -192,23 +192,23 @@ LOGGING = {
     "disable_existing_loggers": False,
     "handlers": {
         "console": {"class": "logging.StreamHandler"},
-        "file": {
-            "class": "logging.FileHandler",
-            "filename": "general.log",
-            "formatter": "verbose",
-        },
-    },
-    "loggers": {
-        "": {
-            "handlers": ["console", "file"],
-            "level": os.environ.get("DJANGO_LOG_LEVEL", "ERROR"),
-        }
-    },
-    "formatters": {
-        "verbose": {
-            "format": "{asctime} ({levelname}) - {name} - {message}",
-            "style": "{",
-        }
+        #     "file": {
+        #         "class": "logging.FileHandler",
+        #         "filename": "general.log",
+        #         "formatter": "verbose",
+        #     },
+        # },
+        # "loggers": {
+        #     "": {
+        #         "handlers": ["console", "file"],
+        #         "level": os.environ.get("DJANGO_LOG_LEVEL", "ERROR"),
+        #     }
+        # },
+        # "formatters": {
+        #     "verbose": {
+        #         "format": "{asctime} ({levelname}) - {name} - {message}",
+        #         "style": "{",
+        #     }
     },
 }
 
