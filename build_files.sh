29b1bfb8fa89cc396e55d8e8d15ff746363952d6
Emmanuel Makoye <makoye224@gmail.com>
2023-11-25 22:45:52 -0500

    abc

--- build_files.sh
+++ build_files.sh
@@ -11,5 +11,3 @@ python manage.py collectstatic --noinput
 
 # Optionally, you may run other commands, such as compressing assets or creating a production-ready build.
 
-# Move the collected static files to the desired directory
-mv static staticfiles_build
